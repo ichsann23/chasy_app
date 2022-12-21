37f5ce21e5574f7ff8c85db188357c7188c14afa
muhammad ichsan <muhammadichsanduatiga2323@gmail.com>
2022-12-21 19:35:49 +0800

    add padding, margin and break on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,21 +24,24 @@ class MyApp extends StatelessWidget {
           ),
           body: SafeArea(
             child: Container(
-              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.only(
+                  left: 23.0, right: 23.0, top: 0.0, bottom: 0.0),
+              padding: EdgeInsets.only(
+                  left: 0.0, top: 50.0, right: 0.0, bottom: 0.0),
               child: Column(
                 children: [
                   Image(
                     image: AssetImage('assets/images/payment1.png'),
-                    height: 250,
+                    height: 200,
                   ),
                   Text(
                     'Hello save me',
                     style: mainHeader,
                   ),
                   Text(
-                    'lets save me and you will be a rich person in the world',
+                    'lets save me and you will be a rich \n person in the world',
                     style: subHeader,
-                    textAlign: TextAlign.justify,
+                    textAlign: TextAlign.center,
                   )
                 ],
               ),
